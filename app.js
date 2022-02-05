701257f9973cf46a85abce681a2899d9e691a79e
unknown <ukaran11@gmail.com>
2022-02-05 16:10:48 +0530

    Utsav |  Setup

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+let app = express();
+
+const PORT = 3000;
+
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'pug');
+
+app.set('views', path.resolve('./src/views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const router = express.Router();
+app.use(router);
+
+const rootPath = path.resolve('./dist');
+
+app.use(express.static(rootPath));
+
+router.use((err, req, res, next) => {
+    if(err) {
+        return res.send(err.message);
+    }
+});
+
+app.listen(PORT, err => {
+    if(err) return console.log(`Cannot listen on PORT: ${PORT}`);
+    console.log(`Server is listening on http://localhost:${PORT}`);
+})
