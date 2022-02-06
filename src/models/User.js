5c8506a3ee64f740b4daf531723635a98ef09f6c
unknown <ukaran11@gmail.com>
2022-02-06 16:58:53 +0530

    Utsav | Added model User

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+module.exports = sequelize.define('User', {
+    id: {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: Sequelize.STRING(35),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+    },
+})
