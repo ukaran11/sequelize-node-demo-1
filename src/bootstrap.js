9ba4ab9383238d28a48187b203642b07f6224c31
unknown <ukaran11@gmail.com>
2022-02-07 21:05:22 +0530

    Utsav | Created simple user and tweet instance and associations

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,14 +2,23 @@ module.exports = async () => {
     const Tweet  = require('./models/Tweet');
     const User = require('./models/User');
 
+    User.hasMany(Tweet, { as: "Tweets", foreignKey: 'userId' });
+    Tweet.belongsTo(User, { as: "User", foreignKey: 'userId'});
+
     const errHandler = (err) => {
         console.error("Error: ", err);
     }
 
-    const user = await User.create({ username: 'alexdmc', passwd: '1234567890'}).catch(errHandler);
+    const user = await User.create({
+         username: 'alexdmc', 
+         passwd: '1234567890'
+    }).catch(errHandler);
 
-    const tweet = await Tweet.create({ content: 'This is actually the tweet content Tweeted from Iphone', userId: user.id}).catch(errHandler);
-    
+    const tweet = await Tweet.create({ 
+        content: 'This is actually the tweet content Tweeted from Iphone', 
+        userId: user.id
+    }).catch(errHandler);
+
 
 
 }
